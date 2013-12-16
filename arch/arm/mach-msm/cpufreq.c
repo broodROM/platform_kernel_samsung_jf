9b24ec0017ecd01cda31efdc4c1f8b34116560e4
hallengreen <hallengreenn@gmail.com>
2013-12-16 17:12:21 +0100

    CPU: Preparing for CPU OC

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -459,6 +459,8 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 			policy->cpu, cur_freq, table[index].frequency);
 	policy->cur = table[index].frequency;
 
+	policy->cur = 1890000;
+	
 	policy->cpuinfo.transition_latency =
 		acpuclk_get_switch_time() * NSEC_PER_USEC;
 
