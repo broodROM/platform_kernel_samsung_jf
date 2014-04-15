62161e38f1184513b24316bc52750b444bf4be8a
hallengreenn <cuziamblack@gmail.com>
2014-04-15 21:28:11 +0200

    Fix: Unknown int

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7581,7 +7581,6 @@ void __init sched_init(void)
 		rq->online = 0;
 		rq->idle_stamp = 0;
 		rq->avg_idle = 2*sysctl_sched_migration_cost;
-		rq->max_idle_balance_cost = sysctl_sched_migration_cost;
 		rq->cur_freq = 0;
 		rq->max_freq = 0;
 		rq->min_freq = 0;
